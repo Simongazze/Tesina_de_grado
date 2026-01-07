cea93d9f8fedeb18e3986c85ec6aab19fdd3be7b
Simongazze <simongazze@hotmail.com>
2026-01-07 13:48:41 -0300

    corrijo error de matcheo

--- pbp_crudo.R
+++ pbp_crudo.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(stringr)
 library(writexl)
 
 c = data.frame(a = unique(pbp.crudo.temporada$accion))
@@ -19,3 +20,21 @@ pbp.crudo.temporada %>% semi_join(b, by = "partido_key") %>%
   count(partido_key)
 
 write_xlsx(c, "acciones.xlsx")
+
+maximos = pbp_preprocesado_temporada %>%
+             group_by(partido_key)%>%
+             summarise(max(posesion))
+
+partido1 = pbp.crudo.temporada %>%
+  filter(partido_key == "GIMNASIA (CR) vs REGATAS (C) (003/05/2025 20:30)")
+
+partido11 = pbp_preprocesado_temporada %>%
+  filter(partido_key == "GIMNASIA (CR) vs REGATAS (C) (003/05/2025 20:30)")
+
+
+a = partido1 %>%
+  mutate(
+    n_comillas_simples = str_count(quinteto_local, "'")
+  )
+
+partido1$quinteto_local
