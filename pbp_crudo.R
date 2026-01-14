de157bca9634ef4690a6506cc45afb292dc2643d
Simongazze <simongazze@hotmail.com>
2026-01-14 12:41:28 -0300

    avance

--- pbp_crudo.R
+++ pbp_crudo.R
@@ -106,3 +106,124 @@ accion_vacia = df_pbp_final %>%
 
 unique(accion_vacia$accion)
 
+#Revision de posesiones con cantidad de puntos raras
+
+unique(poss_by_poss_temporada$puntos_pos)
+
+#1)-4 y 8 puntos
+
+prueba = poss_by_poss_temporada %>%
+  filter(puntos_pos == -4)
+
+prueba = poss_by_poss_temporada %>%
+  filter(puntos_pos == 8)
+
+prueba1 = poss_by_poss_temporada %>%
+  filter(partido_key == "QUIMSA vs BOCA (019/10/2024 11:30)")
+
+prueba2 = pbp_preprocesado_temporada %>%
+  filter(partido_key == "QUIMSA vs BOCA (019/10/2024 11:30)")
+
+prueba3 = df_pbp_final %>%
+  filter(partido_key == "QUIMSA vs BOCA (019/10/2024 11:30)")
+
+# se detecta una inconsistencia a la hora de cargar los datos, en el partido QUIMSA vs BOCA (019/10/2024 11:30)
+# en el cuarto 3 minutos 02:27 hasta el minuto 00:40 hay un error en el conteo de puntos.
+# Este error proviene de la página web, y fue generado (probablemente) por un error en la 
+# mesa de control. Con el triple de CUELLO, MARTIN NICOLAS a los 40 segundos del tercer cuarto
+# el conteo vuelve al valor correcto, quedan por modificarse las celdas intermedias y el conteo
+# de puntos por posesion
+
+prueba3 = poss_by_poss_temporada %>%
+  filter(puntos_pos == -2)
+
+prueba4 = pbp_preprocesado_temporada %>%
+  filter(partido_key == "OBERA vs OBRAS (027/04/2025 21:00)")
+
+#Error de carga desde cuarto 2 minuto 00:14 a 00:00
+
+prueba5 = pbp_preprocesado_temporada %>%
+  filter(partido_key == "OBERA vs OLIMPICO (LB) (002/02/2025 20:30)")
+
+#Error de carga cuarto 2 minuto 04:15 hasta 03:48
+
+prueba6 = pbp_preprocesado_temporada %>%
+  filter(partido_key == "ZARATE BASKET vs PEÑAROL (MDP) (023/10/2024 21:00)")
+
+#Error de carga cuarto 1 minuto 01:00 a 00:57
+
+prueba14 = df_pbp_final %>%
+  filter(partido_key == "ZARATE BASKET vs PLATENSE (012/11/2024 20:00)", cuarto == 1)
+
+prueba14a = poss_by_poss_temporada %>%
+  filter(partido_key == "ZARATE BASKET vs PLATENSE (012/11/2024 20:00)", cuarto == 1)
+
+#Error de carga cuarto 1 07:46 hasta 07:00, y del 7:30 al 7:28 hay dos CANASTAS DE 2 PUNTOS
+#consecutivas sin haber perdidas o cualquier otra acción de por medio.
+
+# Anotaciones altas
+
+prueba = poss_by_poss_temporada %>%
+  filter(puntos_pos == 7)
+
+prueba7 = pbp_preprocesado_temporada %>%
+  filter(partido_key == "OLIMPICO (LB) vs FERRO (009/12/2024 22:00)")
+
+#hay 3 posesiones en 1, dos canastas de 2 puntos y un triple. El tema es que no hay ninguna accion 
+#del rival entre medio, es rarisimo.
+
+prueba = poss_by_poss_temporada %>%
+  filter(puntos_pos == 6)
+
+prueba8 = df_pbp_final %>%
+  filter(partido_key == "FERRO vs QUIMSA (021/04/2025 16:00)", cuarto == 3) #2 triples
+
+#Revisar FERRO vs QUIMSA (021/04/2025 16:00), en el cuarto 1 y 2 las ultimas acciones
+#de cada cuarto estan en el minuto 4 y 3 aproximadamente, lo que da indicios de errores en
+#la carga de las acciones porque es imposible que no hallá ninguna acción en ese tiempo
+
+prueba9 = df_pbp_final %>%
+  filter(partido_key == "INDEPENDIENTE (O) vs QUIMSA (030/11/2024 20:30)", cuarto == 3)
+
+# En este caso hay un doble, al parecer hay una falta en el momento del tiro al jugador ofensivo que
+# peleaba el rebote, como el equipo estaba en bonus fue a la linea, convirtio el primero,
+# falló el segundo y tomaron el rebote ofensivo, luego convirtieron un triple obteniendo así
+# una única posesión donde se convirtieron 6 puntos.
+
+prueba10 = df_pbp_final %>%
+  filter(partido_key == "OBERA vs BOCA (005/11/2024 21:00)", cuarto == 1) 
+# 2 triples
+
+prueba11 = df_pbp_final %>%
+  filter(partido_key == "OLIMPICO (LB) vs ZARATE BASKET (004/11/2024 22:00)", cuarto == 2)
+#Triple, falta ofensiva que está a solo 4 segundos del triple por lo q no se cuenta en la base, y otro triple
+
+prueba12 = df_pbp_final %>%
+  filter(partido_key == "PLATENSE vs OBRAS (026/03/2025 20:30)", cuarto == 1)
+#2 triples
+
+prueba13 = df_pbp_final %>%
+  filter(partido_key == "REGATAS (C) vs GIMNASIA (CR) (002/12/2024 21:30)", cuarto == 2)
+#triple, doble y luego libre, muy raro, deberian ser 2 posesiones triple y luego las otras
+
+#Faltaria revisar los de 5 puntos pero es claro que los errores son de carga
+
+prueba = poss_by_poss_temporada %>%
+  filter(puntos_pos == 5)
+
+#diferencia de tiempo entre acciones PENDIENTE
+
+#Cantidad de puntos por posesion
+
+a = poss_by_poss_temporada %>%
+      group_by(puntos_pos) %>%
+      count() 
+
+sum(a$n*a$puntos_pos)
+
+#Yo creo que si soluciono los negativos y los de 6,7 y 8 puntos estaría bien, quizas los de 5 y 4 tengan varios errores pero debere convivir con ellos
+
+#podriamos llevar el conteo de puntos de manera diferente para evitar estos problemas
+
+
+
